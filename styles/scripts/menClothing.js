e21e1d2bb2b107c975cbe51ab982abf54bd1cbd8
Shivani Pandey <104144826+shivanipandey22@users.noreply.github.com>
2022-06-18 13:19:43 +0530

    initialcommit

---
+++ styles/scripts/menClothing.js
@@ -0,0 +1,284 @@
+var data=[
+    {
+       image:"https://www.yoox.com/images/items/12/12802961wt_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+       name:"PALM ANGELS",
+       type:"T-shirts",
+       price:"840",
+       strikedoffprice:"532",
+       size:"S  M  L" 
+    },
+    {
+        image:"https://www.yoox.com/images/items/13/13787644se_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"VALENTINO",
+        type:"Denim pants",
+        price:"900",
+        strikedoffprice:"500",
+        size: " 30"    
+     },
+     {
+        image:"https://www.yoox.com/images/items/12/12741167il_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"DSQUARED2",
+        type:"T-shirts",
+        price:"700",
+        strikedoffprice:"400",
+        size:"6  7  9" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/17/17257268pq_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"SALVATORE FERRAGAMO",
+        type:"Sneakers",
+        price:"870",
+        strikedoffprice:"502",
+        size:"S  M " 
+     },
+     {
+        image:"https://www.yoox.com/images/items/13/13767828rs_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"FENDI",
+        type:"Denim pants",
+        price:"1020",
+        strikedoffprice:"300",
+        size:"L  XL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/17/17275940hk_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"VALENTINO",
+        type:"Sneakers",
+        price:"849",
+        strikedoffprice:"532",
+        size:"S  M  L" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/13/13782498ku_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"OFF-WHITE",
+        type:"Shorts & Bermuda",
+        price:"303",
+        strikedoffprice:"101",
+        size:"XL  XXL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/12/12782985nw_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"DSQUARED2",
+        type:"Denim shirts",
+        price:"840",
+        strikedoffprice:"532",
+        size:"M  L  XL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/12/12786154di_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"MISSONI",
+        type:"Shirts",
+        price:"900",
+        strikedoffprice:"300",
+        size:"S  L" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/12/12809695os_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"HERON PRESTON",
+        type:"Denim pants",
+        price:"3094",
+        strikedoffprice:"2098",
+        size:"XL  XXL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/17/17275305ou_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"SPRAYGROUND",
+        type:"Sneakers",
+        price:"506",
+        strikedoffprice:"65",
+        size:"S  M" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/13/13786474uo_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"VALENTINO GARAVANI",
+        type:"Shorts & Bermuda",
+        price:"4098",
+        strikedoffprice:"3078",
+        size:"M  S  XL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/17/17274735io_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"LOEWE",
+        type:"Boots",
+        price:"543",
+        strikedoffprice:"67",
+        size:"S  M  L" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/55/55023134kd_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"MARNI",
+        type:"Handbags",
+        price:"600",
+        strikedoffprice:"356",
+        size:"M  XL  XXL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/46/46840508ms_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"VALENTINO",
+        type:"Document holder",
+        price:"560",
+        strikedoffprice:"432",
+        size:"S  M  L" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/12/12821796rq_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"OFF-WHITE",
+        type:"Striped t-shirt",
+        price:"897",
+        strikedoffprice:"456",
+        size:"M  L  XL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/45/45669609bl_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"VALENTINO GARAVANI",
+        type:"Handbags",
+        price:"6785",
+        strikedoffprice:"5567",
+        size:"S  M" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/13/13793333ao_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"SALVATORE FERRAGAMO",
+        type:"Shirts",
+        price:"459",
+        strikedoffprice:"56",
+        size:"S  M  L" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/45/45661318et_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"DOLCE & GABBANA",
+        type:"Backpacks",
+        price:"667",
+        strikedoffprice:"354",
+        size:"M  L  XL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/13/13778364if_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"BURBERRY",
+        type:"Pants",
+        price:"987",
+        strikedoffprice:"234",
+        size:"S  M  XL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/17/17259676qj_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"SALVATORE FERRAGAMO",
+        type:"Boots",
+        price:"786",
+        strikedoffprice:"567",
+        size:"M   XXL" 
+     },
+     {
+        image:"https://www.yoox.com/images/items/46/46842986hx_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        name:"DSQUARED2",
+        type:"Hats",
+        price:"987",
+        strikedoffprice:"344",
+        size:"M  XXL" 
+     },
+]
+localStorage.setItem("menData",JSON.stringify("data"))
+
+var men =JSON.parse(localStorage.getItem("data"))|| []
+display(men)
+var flag= true;
+document.querySelector("#dress1").addEventListener("click",function (){
+    if(flag){
+        document.querySelector(".hide").style.display = "none";
+        return(flag=false);
+    }
+    else{
+        document.querySelector(".hide").style.display = "block"; 
+        return(flag = true);
+    }
+});
+var flag2 = true;
+document.querySelector("#dress2").addEventListener("click", function () {
+    if (flag2) {
+        document.querySelector(".hide2").style.display = "none";
+        return (flag2 = false);
+    } else {
+        document.querySelector(".hide2").style.display = "block";
+        return (flag2 = true);
+    }
+});
+function display(data) {
+    console.log("data")
+    data.map(function (el) {
+        var card = document.createElement("div");
+        card.setAttribute("id", "card")
+
+        var image = document.createElement("img");
+        image.src = el.image;
+
+        var h3 = document.createElement("h5");
+        h3.textContent = el.name;
+
+        var h1 = document.createElement("p");
+        h1.textContent = el.type;
+
+        var p1 = document.createElement("p")
+        p1.textContent =`$${ el.price}`;
+        p1.setAttribute("class", "para")
+
+        var p2 = document.createElement("p")
+        p2.textContent = `$${el.strikedoffprice}`;
+        p2.setAttribute("class","p2")
+
+        var p3 = document.createElement("img")
+        p3.src="https://www.yoox.com/media/yoox14/sections/sr/badge/badge_new_2018n.png"
+        p3.setAttribute("class","symbol2")
+
+        var p4 = document.createElement("p")
+        p4.textContent=el.type
+
+        var p5 = document.createElement("p")
+        p5.textContent=el.size;
+
+        var eyeoff = document.createElement("img")
+        eyeoff.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbySFNtQbxNnSnMs4bSoA8pL_pL5R75XdqGA&usqp=CAU"
+        eyeoff.setAttribute("class","symbol1")
+
+        var fav=document.createElement("img")
+        fav.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQobfSOCPduYpVTa2kAHaLWapi651J_Sp6yTQ&usqp=CAU"
+       fav.setAttribute("class","symbol1")
+       fav.addEventListener("click",favr)
+   
+        
+
+        var div2 = document.createElement("div")
+        div2.setAttribute("class","box2")
+
+        var disc = document.createElement("p");
+        disc.textContent=` ${Math.round((el.price-el.strikedoffprice)/el.price*100)}% Off`
+        disc.setAttribute("class","disc")
+
+
+
+        var div3 = document.createElement("div");
+        div3.setAttribute("id", "price")
+
+        var div4=document.createElement("div")
+        div4.setAttribute("id", "symbol")
+
+        div3.append(p1, disc)
+        div2.append(h3, h1, div3,p2)
+        div4.append(eyeoff,fav,p3)
+        card.append(image,div4,div2,p5);
+
+        card.addEventListener("click", zoomin)
+        function zoomin() {
+            localStorage.setItem("zoomin", JSON.stringify(el))
+            window.location.href = "menProduct.html"
+        }
+
+        document.querySelector("#dressdiv").append(card) 
+       
+        function favr(){
+            fav.style.color="red"
+            alert("Product is added to favourite Page")
+            localStorage.setItem("favData",JSON.stringify(el))
+            window.location.href="fav.html";
+        }
+    })
+}
+display(data);
