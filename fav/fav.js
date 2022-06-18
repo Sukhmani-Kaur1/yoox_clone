ded1bdc7e02d19b8dfa63fc2977d1a63dd4ae525
unknown <sukhmani.dev006@gmail.com>
2022-06-18 12:59:42 +0530

    bugs fixed

--- fav/fav.js
+++ fav/fav.js
@@ -1,44 +1,43 @@
 let getData = JSON.parse(localStorage.getItem("favData"));
 
-let arr = [];
+let arr = JSON.parse(localStorage.getItem("Data")) || [];
 let container = document.getElementById("bag-items");
 //add conditions for null data
 append(getData);
 function append(data) {
-    data.forEach((el, i) => {
-        let div = document.createElement("div");
-        div.className = "box-fav";
-        let img = document.createElement("img");
-        img.src=el.img;
-        let title = document.createElement("h4");
+  data.forEach((el, i) => {
+    let div = document.createElement("div");
+    div.className = "box-fav";
+    let img = document.createElement("img");
+    img.src = el.img;
+    let title = document.createElement("h4");
     title.textContent = el.title;
-        let remove = document.createElement("button");
-        remove.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-        remove.addEventListener("click", function () {
-            data.splice(i, 1);
-            localStorage.setItem("favData", JSON.stringify(data));
-            window.location.reload();
-          });
-          let addCart = document.createElement("a");
-          addCart.textContent="ADD TO CART"
-        //   addCart.href="./cart/cart.html"
-          addCart.addEventListener("click",function(){
-            if(!arr.includes(el)){
-                arr.push(el)
-                localStorage.setItem("data",JSON.stringify(arr))
-            }else{
-                alert("already in the cart")
-            }
-            // console.log(arr)
-          })
-        div.append(img,remove,title,addCart)
-        container.append(div)
+    let remove = document.createElement("button");
+    remove.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+    remove.addEventListener("click", function () {
+      data.splice(i, 1);
+      localStorage.setItem("favData", JSON.stringify(data));
+      window.location.reload();
     });
+    let addCart = document.createElement("a");
+    addCart.textContent = "ADD TO CART";
+    //   addCart.href="./cart/cart.html"
+    addCart.addEventListener("click", function () {
+      if (!arr.includes(el)) {
+        arr.push(el);
+        localStorage.setItem("Data", JSON.stringify(arr));
+      } else {
+        alert("already in the cart");
+      }
+      // console.log(arr)
+    });
+    div.append(img, remove, title, addCart);
+    container.append(div);
+  });
 }
 let selection = document.getElementById("select");
 
-selection.addEventListener("change",function(){
-    let input = selection.value
-    console.log(input)
-    
-})
+selection.addEventListener("change", function () {
+  let input = selection.value;
+  console.log(input);
+});
