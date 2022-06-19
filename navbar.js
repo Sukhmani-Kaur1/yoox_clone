df96ba3e76488fe42fe01accae2a0b5480b2513a
suranjanachary <101392637+suranjanachary@users.noreply.github.com>
2022-06-19 20:27:56 +0530

    final

--- navbar.js
+++ navbar.js
@@ -1,5 +1,5 @@
 function navbar(){
-    return ` <div>
+    return `  <div>
     <!-- navbar1 -->
 
     <div id="nav1">
@@ -8,9 +8,9 @@ function navbar(){
             <div id="cust_care"><p>CUSTOMER CARE</p> </div>
         </div>
         <div id="nav1_right">
-            <div id="reg"><i class="material-icons">edit</i><p > <a href="signup.html" > REGISTER</a>
+            <div id="reg"><i class="material-icons">edit</i><p > <a href="./signup.html" > REGISTER</a>
                </p></div>
-            <div id="log"><i class="material-icons">lock_open</i><p> <a href="signin.html" >LOGIN</a></p></div>
+            <div id="log"><i class="material-icons">lock_open</i><p> <a href="./signin.html" >LOGIN</a></p></div>
         </div>
     </div>
 
@@ -18,19 +18,19 @@ function navbar(){
 
     <div id="nav2">
         <div id="nav2_left">
-            <div id="women"><p><a href="../yoox_clone/womenClothing.html">WOMEN</a> </p></div>
-            <div id="men"><p><a href="../yoox_clone/menClothing.html">MEN</a> </p></div>
+            <div id="women"><p><a href="./womenClothing.html">WOMEN</a> </p></div>
+            <div id="men"><p><a href="./menClothing.html">MEN</a> </p></div>
             <div id="kids"><p>KIDS /</p></div>
-            <div id="design_art"><p><a href="#">DESIGN+ART</a> </p></div>
+            <div id="design_art"><p><a href="./index.html">DESIGN+ART</a> </p></div>
         </div>
         <div id="YooxLogo">
-          <img src="../yoox_clone/images/yoox logo.png">
+            <img src="images/yoox logo.png" alt="">
         </div>
         <input type="text" id="search_bar">
         <div id="nav2_right">
             <i id="click_btn" class="material-icons">search</i>
-           <a href="../yoox_clone/fav.html"> <i id="fav_bag" class="material-icons">favorite_border</i><span id="fav_count">0</span></a>
-           <a href="../yoox_clone/cart.html"> <i id="shopping_bag" class="material-icons">shopping_bag</i><span id="bag_count">0</span></a>
+           <a href="./fav.html"> <i id="fav_bag" class="material-icons">favorite_border</i><span id="fav_count">0</span></a>
+           <a href="./cart.html"> <i id="shopping_bag" class="material-icons">shopping_bag</i><span id="bag_count">0</span></a>
             
         </div>
     
